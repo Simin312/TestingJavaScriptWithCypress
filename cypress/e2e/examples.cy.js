828e261454c4e3f881c15d202ec6a9af9943b8ef
unknown <siminchan312@gmail.com>
2024-02-17 22:13:42 +0800

    test the cy.within() method to get the element under specific data-test name.

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -17,16 +17,18 @@ describe('Various example',()=>{
         cy.getDataTest('nav-forms').click();
         cy.location('pathname').should("equal","/forms")
 
-        cy.getDataTest('nav-examples').click();
-        cy.location('pathname').should("equal","/examples")
-
         cy.getDataTest('nav-component').click();
         cy.location('pathname').should("equal","/component")
 
         cy.getDataTest('nav-best-practices').click();
         cy.location('pathname').should("equal","/best-practices")
+        
+        cy.getDataTest('nav-examples').click();
+        cy.location('pathname').should("equal","/examples")
+    
     })
-    it.only('intercept', ()=>{
+
+    it('intercept', ()=>{
         cy.intercept("POST", 'http://localhost:3000/examples', {
             body:{
                 message: 'successfully intercepted request'
@@ -34,4 +36,54 @@ describe('Various example',()=>{
         })
         cy.getDataTest('post-button').click()
     })
+
+    it.only('grudges',()=>{
+        cy.contains(/add some grudges/i)
+        // add grudge
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length',0)
+        })
+
+        // check clear button should not exist
+        cy.getDataTest('clear-button').should('not.exist')
+
+        // check the grudge title
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+
+        // add grudge
+        cy.getDataTest('grudge-input').within(()=>{
+            cy.get('input').type('some grudge')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length',1)
+        })
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Grudges')
+
+        // add grudge
+        cy.getDataTest('grudge-input').within(()=>{
+            cy.get('input').type('some another grudge')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length',2)
+            cy.get('li').its(0).should('contain.text','some grudge')
+        })
+
+        // delete first grudge
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').its(0).within(()=>{
+                cy.get('button').click()
+            })
+        })
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length',1)
+        })
+
+        cy.getDataTest('clear-button').click()
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length',0)
+        })
+    })  
 })
